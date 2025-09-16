4b8d6bfbf4916110d4f93334188f32e361f04cf5
Nate <oneblurryeye@gmail.com>
2025-09-16 18:19:58 -0400

    Removed work exp

--- app/data.ts
+++ app/data.ts
@@ -3,15 +3,7 @@ type Project = {
   description: string
   link: string
   video: string
-  id: string
-}
-
-type WorkExperience = {
-  company: string
-  title: string
-  start: string
-  end: string
-  link: string
+  image: string
   id: string
 }
 
@@ -32,45 +24,18 @@ export const PROJECTS: Project[] = [
     name: 'GameKnight',
     description:
       'Crown your crew.',
-    link: 'https://figma.com/',
-    video:
-      'https://res.cloudinary.com/read-cv/video/upload/t_v_b/v1/1/profileItems/W2azTw5BVbMXfj7F53G92hMVIn32/newProfileItem/d898be8a-7037-4c71-af0c-8997239b050d.mp4?_a=DATAdtAAZAA0',
+    link: '#',
+    video: '',
+    image: '',
     id: 'project1',
   },
   {
     name: 'ChoreBud',
     description: 'Making chores easier.',
-    link: 'https://figma.com/',
-    video:
-      'https://res.cloudinary.com/read-cv/video/upload/t_v_b/v1/1/profileItems/W2azTw5BVbMXfj7F53G92hMVIn32/XSfIvT7BUWbPRXhrbLed/ee6871c9-8400-49d2-8be9-e32675eabf7e.mp4?_a=DATAdtAAZAA0',
+    link: '#',
+    video: '',
+    image: '',
     id: 'project2',
-  },
-]
-
-export const WORK_EXPERIENCE: WorkExperience[] = [
-  {
-    company: 'Reglazed Studio',
-    title: 'CEO',
-    start: '2024',
-    end: 'Present',
-    link: 'https://ibelick.com',
-    id: 'work1',
-  },
-  {
-    company: 'Freelance',
-    title: 'Design Engineer',
-    start: '2022',
-    end: '2024',
-    link: 'https://ibelick.com',
-    id: 'work2',
-  },
-  {
-    company: 'Freelance',
-    title: 'Front-end Developer',
-    start: '2017',
-    end: 'Present',
-    link: 'https://ibelick.com',
-    id: 'work3',
   },
 ]
 
