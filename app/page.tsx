670e45fae3de64937711f0764fddbb31da2db048
Nate <oneblurryeye@gmail.com>
2025-09-17 07:59:34 -0400

    Updated intro bio

--- app/page.tsx
+++ app/page.tsx
@@ -136,8 +136,8 @@ export default function Personal() {
       >
         <div className="flex-1">
           <p className="text-zinc-600 dark:text-zinc-400">
-            Focused on creating intuitive and performant web experiences.
-            Bridging the gap between design and development.
+            Passionate about untangling complex problems to create 
+            simple, intuitive, and effective digital experiences.
           </p>
         </div>
       </motion.section>
