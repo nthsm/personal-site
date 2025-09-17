d9d9c1def5fb1b4aa57bfd691afc1dc212aadd84
Nate <oneblurryeye@gmail.com>
2025-09-17 14:50:17 -0400

    Update data.ts

--- app/data.ts
+++ app/data.ts
@@ -1,17 +1,9 @@
-type Project = {
+type CaseStudy = {
   name: string
   description: string
   link: string
-  video: string
   image: string
   id: string
-}
-
-type BlogPost = {
-  title: string
-  description: string
-  link: string
-  uid: string
 }
 
 type SocialLink = {
@@ -19,72 +11,37 @@ type SocialLink = {
   link: string
 }
 
-export const PROJECTS: Project[] = [
+export const CASE_STUDIES: CaseStudy[] = [
   {
     name: 'GameKnight',
     description:
       'Crown your crew.',
     link: '#',
-    video: '',
-    image: '',
+    image: '/placeholder.png',
     id: 'project1',
   },
   {
     name: 'ChoreBud',
     description: 'Making chores easier.',
     link: '#',
-    video: '',
-    image: '',
+    image: '/placeholder.png',
     id: 'project2',
-  },
-]
-
-export const BLOG_POSTS: BlogPost[] = [
-  {
-    title: 'Exploring the Intersection of Design, AI, and Design Engineering',
-    description: 'How AI is changing the way we design',
-    link: '/blog/exploring-the-intersection-of-design-ai-and-design-engineering',
-    uid: 'blog-1',
-  },
-  {
-    title: 'Why I left my job to start my own company',
-    description:
-      'A deep dive into my decision to leave my job and start my own company',
-    link: '/blog/exploring-the-intersection-of-design-ai-and-design-engineering',
-    uid: 'blog-2',
-  },
-  {
-    title: 'What I learned from my first year of freelancing',
-    description:
-      'A look back at my first year of freelancing and what I learned',
-    link: '/blog/exploring-the-intersection-of-design-ai-and-design-engineering',
-    uid: 'blog-3',
-  },
-  {
-    title: 'How to Export Metadata from MDX for Next.js SEO',
-    description: 'A guide on exporting metadata from MDX files to leverage Next.js SEO features.',
-    link: '/blog/example-mdx-metadata',
-    uid: 'blog-4',
   },
 ]
 
 export const SOCIAL_LINKS: SocialLink[] = [
   {
     label: 'Github',
-    link: 'https://github.com/ibelick',
-  },
-  {
-    label: 'Twitter',
-    link: 'https://twitter.com/ibelick',
+    link: 'https://github.com/nthsm',
   },
   {
     label: 'LinkedIn',
-    link: 'https://www.linkedin.com/in/ibelick',
+    link: 'https://www.linkedin.com/in/nthsm',
   },
   {
     label: 'Instagram',
-    link: 'https://www.instagram.com/ibelick',
+    link: 'https://www.instagram.com/nhtfm',
   },
 ]
 
-export const EMAIL = 'your@email.com'
+export const EMAIL = 'nathan@nthsm.com'
