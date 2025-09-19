cadd5440b9720ffac3640a4fdd9f1f66a569968e
Nate <oneblurryeye@gmail.com>
2025-09-19 19:22:35 -0400

    Added more projects

--- app/data.ts
+++ app/data.ts
@@ -20,6 +20,38 @@ export const CASE_STUDIES: CaseStudy[] = [
     image: '/placeholder.png',
     id: 'project1',
   },
+  {
+    name: 'GameKnight App Concept',
+    description:
+      'A mobile app concept that removes the logistical hurdles of planning a game night, from scheduling to game selection.',
+    link: '/case-studies/gameknight',
+    image: '/placeholder.png',
+    id: 'project2',
+  },
+  {
+    name: 'ChoreBud App Concept',
+    description:
+      'A concept for a mobile app designed to reduce household friction by gamifying and simplifying chore management for roommates.',
+    link: '/case-studies/chorebud',
+    image: '/placeholder.png',
+    id: 'project3',
+  },
+  {
+    name: 'IMON Website Redesign',
+    description:
+      'Revitalizing the digital presence for IMON, involving a complete overhaul of the information architecture and visual identity.',
+    link: '/case-studies/imon',
+    image: '/placeholder.png',
+    id: 'project4',
+  },
+  {
+    name: 'Spotify Road Trip Feature',
+    description:
+      'N/A',
+    link: '/case-studies/spotify-road-trip',
+    image: '/placeholder.png',
+    id: 'project5',
+  },
 ]
 
 export const SOCIAL_LINKS: SocialLink[] = [
