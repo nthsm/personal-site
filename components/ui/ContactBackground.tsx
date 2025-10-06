e51bbcbbc100fe8db14965b2005c392657956d86
Nate <oneblurryeye@gmail.com>
2025-10-06 15:55:23 -0400

    Create ContactBackground.tsx

---
+++ components/ui/ContactBackground.tsx
@@ -0,0 +1,32 @@
+import Image from 'next/image'
+import { Marquee } from './marquee'
+
+const MarqueeRow = ({ reverse = false }: { reverse?: boolean }) => (
+  <Marquee reverse={reverse}>
+    {Array(15)
+      .fill(0)
+      .map((_, i) => (
+        <div key={i} className="relative h-12 w-24 shrink-0 mx-2 md:h-16 md:w-32">
+          <Image
+            src="/ns.svg"
+            alt="NS Logo"
+            layout="fill"
+            objectFit="contain"
+            className="opacity-20 dark:opacity-10"
+          />
+        </div>
+      ))}
+  </Marquee>
+)
+
+export function ContactBackground() {
+  return (
+    <div className="absolute inset-0 z-0 flex flex-col justify-between overflow-hidden">
+      {Array(20)
+        .fill(0)
+        .map((_, i) => (
+          <MarqueeRow key={i} reverse={i % 2 === 1} />
+        ))}
+    </div>
+  )
+}
