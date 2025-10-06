0258ebab57204d006d45ebc19b17c871d39c099f
Nate <oneblurryeye@gmail.com>
2025-10-06 12:19:06 -0400

    Update data.ts

--- app/data.ts
+++ app/data.ts
@@ -1,3 +1,6 @@
+import { SiLinkedin, SiInstagram, SiStrava } from 'react-icons/si';
+import { IconType } from 'react-icons';
+
 export type Project = {
   name: string
   description: string
@@ -9,6 +12,7 @@ export type Project = {
 type SocialLink = {
   label: string
   link: string
+  icon: IconType
 }
 
 export const PROJECTS: Project[] = [
@@ -16,7 +20,7 @@ export const PROJECTS: Project[] = [
     name: 'Sleeper App Redesign',
     description:
       'A project on improving the user experience and visual interface of the popular fantasy sports app, Sleeper.',
-    link: '/projects/sleeper',
+    link: '/sleeper',
     image: '/placeholder1.png',
     id: 'project1',
   },
@@ -24,7 +28,7 @@ export const PROJECTS: Project[] = [
     name: 'GameKnight App Concept',
     description:
       'A mobile app concept that removes the logistical hurdles of planning a game night, from scheduling to game selection.',
-    link: '/projects/gameknight',
+    link: '/gameknight',
     image: '/placeholder2.png',
     id: 'project2',
   },
@@ -32,7 +36,7 @@ export const PROJECTS: Project[] = [
     name: 'ChoreBud App Concept',
     description:
       'A concept for a mobile app designed to reduce household friction by gamifying and simplifying chore management for roommates.',
-    link: '/projects/chorebud',
+    link: '/chorebud',
     image: '/placeholder3.png',
     id: 'project3',
   },
@@ -40,27 +44,26 @@ export const PROJECTS: Project[] = [
     name: 'Spotify Road Trip Feature',
     description:
       'A conceptual feature for Spotify that transforms passive listening into a collaborative, safe, and memorable road trip experience.',
-    link: '/projects/spotify-road-trip',
+    link: '/spotify-road-trip',
     image: '/placeholder4.png',
     id: 'project4',
   },
 ]
 
 export const SOCIAL_LINKS: SocialLink[] = [
-  {
-    label: 'Github',
-    link: 'https://github.com/nthsm',
-  },
-  {
+    {
     label: 'LinkedIn',
     link: 'https://www.linkedin.com/in/nthsm',
+    icon: SiLinkedin,
   },
   {
     label: 'Instagram',
     link: 'https://www.instagram.com/nhtfm',
+    icon: SiInstagram,
   },
   {
     label: 'Strava',
     link: 'https://www.strava.com/athletes/139602024',
+    icon: SiStrava,
   },
 ]
