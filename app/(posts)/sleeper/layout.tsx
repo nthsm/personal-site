9340e7ab5e1b9ee648f60d422da53d75baf07e44
Nate <oneblurryeye@gmail.com>
2025-10-07 10:44:55 -0400

    Updated layout

--- app/(posts)/sleeper/layout.tsx
+++ app/(posts)/sleeper/layout.tsx
@@ -1,10 +1,28 @@
 import type { Metadata } from 'next'
-import { title, description } from './page.mdx' 
 import { ReactNode } from 'react'
+import path from 'path'
+import { promises as fs } from 'fs'
+import matter from 'gray-matter'
 
-export const metadata: Metadata = {
-  title,
-  description,
+export async function generateMetadata(): Promise<Metadata> {
+  const relativePath = 'app/(posts)/sleeper/page.mdx'
+  const fullPath = path.join(process.cwd(), relativePath)
+
+  let data: { title?: string; description?: string } = {}
+  try {
+    const fileContent = await fs.readFile(fullPath, 'utf8')
+    data = matter(fileContent).data
+  } catch (error) {
+    
+  }
+
+  const projectTitle = (data.title ?? 'Untitled Project')
+  const projectDescription = data.description
+
+  return {
+    title: `${projectTitle} — Nathan Smith`,
+    description: projectDescription,
+  }
 }
 
 export default function ProjectLayout({ children }: { children: ReactNode }) {
