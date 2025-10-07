2126c2f51ded3229eaee06e5ce5eef3e5b74a60b
Nate <oneblurryeye@gmail.com>
2025-10-06 23:59:17 -0400

    Update layout.tsx

--- app/(posts)/layout.tsx
+++ app/(posts)/layout.tsx
@@ -1,26 +1,55 @@
-'use client'
-import Link from 'next/link'
-import { ArrowLeft } from 'lucide-react'
-import { SiteLayout } from '@/components/ui/SiteLayout'
+import type { Metadata } from 'next'
+import { promises as fs } from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
+import { PROJECTS } from '@/app/data'
+import { ReactNode } from 'react'
 
-export default function LayoutBlogPost({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <SiteLayout showProgressBar>
-      <main className="prose prose-zinc mx-auto pb-20 dark:prose-invert prose-h1:text-2xl prose-h1:font-semibold max-w-none">
-        <Link
-          href="/"
-          className="no-underline mb-12 inline-flex items-center gap-2 text-sm text-zinc-600 transition-colors hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-zinc-50"
-        >
-          <ArrowLeft className="h-4 w-4" />
-          <span>Back to Projects</span>
-        </Link>
-        
-        {children}
-      </main>
-    </SiteLayout>
+type Params = {
+  slug: string
+}
+
+async function getPostMetadata(slug: string) {
+  const MDX_FILE_PATH = path.join(
+    process.cwd(), 
+    'app', 
+    '(posts)', 
+    '[slug]', 
+    `${slug}.mdx` 
   )
+
+  try {
+    const fileContents = await fs.readFile(MDX_FILE_PATH, 'utf8')
+    const { data } = matter(fileContents)
+    return {
+        title: data.title,
+        description: data.description,
+    }
+  } catch (error) {
+    return null;
+  }
+}
+
+export function generateStaticParams() {
+  return PROJECTS.map(project => ({
+    slug: project.link.slice(1), 
+  }))
+}
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Params
+}): Promise<Metadata> {
+  const metadata = await getPostMetadata(params.slug);
+  
+  if (metadata) {
+    return metadata as Metadata;
+  }
+  
+  return { title: 'Project Not Found' };
+}
+
+export default function DynamicPostLayout({ children }: { children: ReactNode }) {
+  return children
 }
