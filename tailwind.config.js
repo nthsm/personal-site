7085324a9a8d6d70418150f7970475ed51c82d51
Nate <oneblurryeye@gmail.com>
2025-10-08 11:35:49 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
@@ -6,6 +7,20 @@ module.exports = {
   ],
   theme: {
     extend: {
+      screens: {
+        // Default breakpoints for mobile and tablets
+        'sm': '640px',   // Small tablets & landscape phones
+        'md': '768px',   // Tablets
+        'lg': '1024px',  // Laptops & small desktops
+
+        // Standard 16:9 Desktops
+        'xl': '1280px',  // HD & some FHD
+        '2xl': '1536px', // FHD & QHD monitors
+
+        // Custom Ultrawide Breakpoints
+        'uw': '2560px',   // 21:9 Ultrawide (e.g., 2560x1080, 3440x1440)
+        'suw': '3840px',  // 32:9 Super Ultrawide (e.g., 3840x1080, 5120x1440)
+      },
       backdropBlur: {
         'xs': '2px',
       }
