1977dbf65b94899628091f768818e518226e9766
Nate <oneblurryeye@gmail.com>
2025-10-10 14:29:23 -0400

    Update layout.tsx

--- app/(posts)/layout.tsx
+++ app/(posts)/layout.tsx
@@ -11,13 +11,13 @@ export default function LayoutBlogPost({
 }) {
   return (
     <SiteLayout showProgressBar>
-      <main className="prose prose-zinc mx-auto pb-20 dark:prose-invert prose-h1:text-2xl prose-h1:font-semibold max-w-none">
+      <main className="prose prose-zinc mx-auto max-w-5xl pb-20 dark:prose-invert prose-h1:font-serif prose-h1:text-5xl prose-h1:tracking-tight">
         <Link
           href="/"
           className="no-underline mb-12 inline-flex items-center gap-2 text-sm text-zinc-600 transition-colors hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-zinc-50"
         >
           <ArrowLeft className="h-4 w-4" />
-          <span>Back to Projects</span>
+          <span>Back to Home</span>
         </Link>
         
         {children}
